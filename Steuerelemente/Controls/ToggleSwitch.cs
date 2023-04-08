0b85f8e83ee15fc8ea9164ec4555406d019cd239
Jan Wollny <Jan@Wollny.Hamburg>
2023-04-08 10:44:47 +0200

    Projektdateien hinzufügen.

---
+++ Steuerelemente/Controls/ToggleSwitch.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Steuerelemente
+{
+    /// <summary>
+    /// Führen Sie die Schritte 1a oder 1b und anschließend Schritt 2 aus, um dieses benutzerdefinierte Steuerelement in einer XAML-Datei zu verwenden.
+    ///
+    /// Schritt 1a) Verwenden des benutzerdefinierten Steuerelements in einer XAML-Datei, die im aktuellen Projekt vorhanden ist.
+    /// Fügen Sie dieses XmlNamespace-Attribut dem Stammelement der Markupdatei 
+    /// an der Stelle hinzu, an der es verwendet werden soll:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Steuerelemente"
+    ///
+    ///
+    /// Schritt 1b) Verwenden des benutzerdefinierten Steuerelements in einer XAML-Datei, die in einem anderen Projekt vorhanden ist.
+    /// Fügen Sie dieses XmlNamespace-Attribut dem Stammelement der Markupdatei 
+    /// an der Stelle hinzu, an der es verwendet werden soll:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Steuerelemente;assembly=Steuerelemente"
+    ///
+    /// Darüber hinaus müssen Sie von dem Projekt, das die XAML-Datei enthält, einen Projektverweis
+    /// zu diesem Projekt hinzufügen und das Projekt neu erstellen, um Kompilierungsfehler zu vermeiden:
+    ///
+    ///     Klicken Sie im Projektmappen-Explorer mit der rechten Maustaste auf das Zielprojekt und anschließend auf
+    ///     "Verweis hinzufügen"->"Projekte"->[Navigieren Sie zu diesem Projekt, und wählen Sie es aus.]
+    ///
+    ///
+    /// Schritt 2)
+    /// Fahren Sie fort, und verwenden Sie das Steuerelement in der XAML-Datei.
+    ///
+    ///     <MyNamespace:ToggleSwitch/>
+    ///
+    /// </summary>
+    public class ToggleSwitch : ToggleButton
+    {
+        static ToggleSwitch()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleSwitch), new FrameworkPropertyMetadata(typeof(ToggleSwitch)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        public ToggleSwitch() { }
+
+
+
+        private static CornerRadius _defaultCornerRadius = new CornerRadius(25.0);
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(ToggleSwitch), new PropertyMetadata(_defaultCornerRadius));
+
+
+
+
+        private static Brush _onColor = Brushes.Green;
+        public Brush OnColor
+        {
+            get { return (Brush)GetValue(OnColorProperty); }
+            set { SetValue(OnColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OnColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OnColorProperty =
+            DependencyProperty.Register(nameof(OnColor), typeof(Brush), typeof(ToggleSwitch), new PropertyMetadata(_onColor));
+
+        private static Brush _offColor = Brushes.Red;
+        public Brush OffColor
+        {
+            get { return (Brush)GetValue(OffColorProperty); }
+            set { SetValue(OffColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OnColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OffColorProperty =
+            DependencyProperty.Register(nameof(OffColor), typeof(Brush), typeof(ToggleSwitch), new PropertyMetadata(_offColor));
+    }
+}
---
+++ Steuerelemente/UserControl1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Steuerelemente
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl, INotifyPropertyChanged
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        private static CornerRadius _defaultCornerRadius = new CornerRadius(25.0);
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(UserControl1), new PropertyMetadata(_defaultCornerRadius));
+
+
+
+
+        private static Brush _onColor = Brushes.Green;
+        public Brush OnColor
+        {
+            get { return (Brush)GetValue(OnColorProperty); }
+            set { SetValue(OnColorProperty, value); PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OnColor")); }
+        }
+
+        // Using a DependencyProperty as the backing store for OnColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OnColorProperty =
+            DependencyProperty.Register(nameof(OnColor), typeof(Brush), typeof(UserControl1), new PropertyMetadata(_onColor));
+
+        private static Brush _offColor = Brushes.Red;
+        public Brush OffColor
+        {
+            get => (Brush)GetValue(OffColorProperty);
+            set { SetValue(OffColorProperty, value); PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OffColor")); }
+        }
+
+        // Using a DependencyProperty as the backing store for OnColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OffColorProperty =
+            DependencyProperty.Register(nameof(OffColor), typeof(Brush), typeof(UserControl1), new PropertyMetadata(_offColor));
+
+
+
+
+        private static Thickness _defaultThickness = new Thickness(1);
+        public Thickness myBorderThickness
+        {
+            get { return (Thickness)GetValue(myBorderThicknessProperty); }
+            set { SetValue(myBorderThicknessProperty, value); PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("myBorderThickness")); }
+        }
+
+        // Using a DependencyProperty as the backing store for BorderThickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty myBorderThicknessProperty =
+            DependencyProperty.Register(nameof(myBorderThickness), typeof(Thickness), typeof(UserControl1), new PropertyMetadata(_defaultThickness));
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
